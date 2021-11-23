ff1dc68e9a07fd3895f7525c991cbb331cce493f
Ubuntu <ubuntu@ip-172-31-30-99.ec2.internal>
2021-11-23 18:03:52 +0000

    fixed errors in linter

--- Logs.java
+++ Logs.java
@@ -1,22 +1,26 @@
-  /**
-  * The log program takes log length and gives you the amount of logs that can fit in a truck
-  *
-  *  @author Jakob
-  *  @version 1.0
-  *  @since 2020-11-22
-  *   Class Logs
-  */
+/*
+* The log program takes log length and gives you the amount of logs that can fit in a truck.
+*
+* @author Jakob
+* @version 1.0
+* @since 2020-11-22
+* Class Logs.
+*/
 
 import java.util.Scanner;
 
 /**
-*  The logs program tells you how many logs a truck can carry depending on the log size o 1m, .5m or .25m.
-*
+*  The logs program tell how many logs a truck can carry depending on the log size 1m, .5m or .25m.
 *  A log weighs 20kg/m and the truck can carry 1100kg
-*
 */
 
 final class Logs {
+
+    /** Density of logs. */
+    public static final double DENSITY = 20.0;
+
+    /** Max truck load.*/
+    public static final double TRUCK_LOAD = 1100;
 
     /**
     * Prevents instantiation.
@@ -36,51 +40,25 @@ final class Logs {
     *
     */
 
-    /**
-    *  Density of logs in kg/m
-    */
-
-    public static final double DENSITY = 20.0;
-
-    /**
-    * Max truck load
-    */
-
-    public static final double TRUCK_LOAD = 1100;
-
     public static void main(final String[] args) {
-        //variables
+
+        // variables
         final double numLogs;
 
         final Scanner myObj = new Scanner(System.in);
-        //get user input
+        // get user input
 
-        System.out.println("Enter log length (0.25, .0 or 1): ");
-        //stores float in length
+        System.out.println("Enter log length (0.25, .5 or 1): ");
+        // stores float in length
 
         final float length = myObj.nextFloat();
-        //num of logs calculator
+        // num of logs calculator
 
-        numLogs = (TRUCK_LOAD / length) / DENSITY
-        
+        numLogs = (TRUCK_LOAD / length) / DENSITY;
 
-        System.out.println("The truck will be able to take " + TRUCK_LOAD "logs that are " + length "meter(s) in length.")
-        System.out.println("\nDone")
-        }
+        // outputs amount of logs
+        System.out.println("The truck will be able to take " + numLogs + " logs that are "
+            + length + " meter(s) in length.");
+        System.out.println("\nDone");
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
